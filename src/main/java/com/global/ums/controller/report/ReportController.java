1cc7f239b60b71c7556b5f014c6bd05b9cfbe91b
赵佳鹏 <1939291579@qq.com>
2025-12-01 14:43:44 +0800

    推送微信相关

--- src/main/java/com/global/ums/controller/report/ReportController.java
+++ src/main/java/com/global/ums/controller/report/ReportController.java
@@ -2,74 +2,177 @@ package com.global.ums.controller.report;
 
 import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import com.global.ums.constant.UserPropertiesConstant;
+import com.global.ums.dto.AlarmPushDTO;
+import com.global.ums.entity.User;
+import com.global.ums.entity.UserGroup;
+import com.global.ums.entity.UserProperties;
+import com.global.ums.enums.UserType;
+import com.global.ums.result.AjaxResult;
+import com.global.ums.service.UserGroupService;
+import com.global.ums.service.UserPropertiesService;
+import com.global.ums.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * 认证控制器
+ * 报告控制器
  */
+@Slf4j
 @RestController
 @RequestMapping("/report")
+@RequiredArgsConstructor
 public class ReportController {
-    @Autowired
+
     private final WxMpService wxMpService;
-
-    @Autowired
     private final WxMaService wxMaService;
+    private final UserService userService;
+    private final UserGroupService userGroupService;
+    private final UserPropertiesService userPropertiesService;
 
-    public ReportController(WxMpService wxMpService, WxMaService wxMaService) {
-        this.wxMpService = wxMpService;
-        this.wxMaService = wxMaService;
+    /**
+     * 微信模板消息模板ID
+     */
+    private static final String ALARM_TEMPLATE_ID = "RvvWaQI0at6oQ_X3U5x42-kXKdRXvu15x3QGeMWEzBo";
+
+    /**
+     * 推送告警消息到微信公众号
+     * 根据用户组ID遍历所有子节点中type=1且有mp_openid属性的用户进行推送
+     *
+     * @param dto 告警推送请求参数
+     * @return 推送结果
+     */
+    @PostMapping("/push")
+    public AjaxResult pushWechat(@RequestBody @Validated AlarmPushDTO dto) {
+        // 获取用户组下所有需要推送的用户openId列表
+        List<String> openIdList = getOpenIdListByGroupId(dto.getUserId());
+
+        if (openIdList.isEmpty()) {
+            return AjaxResult.error("没有找到需要推送的用户");
+        }
+
+        // 构建模板数据
+        String alarmTime = StrUtil.isNotBlank(dto.getAlarmTime()) ? dto.getAlarmTime() : DateUtil.now();
+        List<WxMpTemplateData> dataList = Arrays.asList(
+                new WxMpTemplateData("thing73", dto.getSystemName()),    // 所属系统
+                new WxMpTemplateData("thing52", dto.getPointName()),     // 测点名称
+                new WxMpTemplateData("time3", alarmTime),                // 告警时间
+                new WxMpTemplateData("thing46", dto.getAlarmType()),     // 告警类型
+                new WxMpTemplateData("thing25", dto.getExceptionReason()) // 异常原因
+        );
+
+        int successCount = 0;
+        int failCount = 0;
+        List<String> failOpenIds = new ArrayList<>();
+
+        // 遍历推送
+        for (int i = 0; i < openIdList.size(); i++) {
+            String openId = openIdList.get(i);
+            try {
+                WxMpTemplateMessage message = new WxMpTemplateMessage();
+                message.setTemplateId(ALARM_TEMPLATE_ID);
+                message.setToUser(openId);
+
+                // 设置小程序跳转
+                WxMpTemplateMessage.MiniProgram miniProgram = new WxMpTemplateMessage.MiniProgram();
+                miniProgram.setAppid(wxMaService.getWxMaConfig().getAppid());
+                miniProgram.setPagePath("pages/index");
+                message.setMiniProgram(miniProgram);
+
+                // 防重入id，确保一次报警只推送一次（每个用户使用不同的clientMsgId）
+                message.setClientMsgId(dto.getClientMsgId() + "_" + openId);
+                message.setData(dataList);
+
+                wxMpService.getTemplateMsgService().sendTemplateMsg(message);
+                successCount++;
+            } catch (WxErrorException e) {
+                log.error("推送消息给用户 {} 失败: {}", openId, e.getMessage());
+                failCount++;
+                failOpenIds.add(openId);
+            }
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("total", openIdList.size());
+        result.put("success", successCount);
+        result.put("fail", failCount);
+        if (!failOpenIds.isEmpty()) {
+            result.put("failOpenIds", failOpenIds);
+        }
+
+        if (failCount == 0) {
+            return AjaxResult.success("推送成功", result);
+        } else if (successCount == 0) {
+            return AjaxResult.error("推送全部失败", result);
+        } else {
+            return AjaxResult.success("部分推送成功", result);
+        }
     }
 
     /**
-     * 用户登录
+     * 根据用户组ID获取所有需要推送的用户openId列表
+     * 递归遍历所有子节点，筛选type=1且有mp_openid属性的用户
+     *
+     * @param groupId 用户组ID
+     * @return openId列表
      */
-    @PostMapping("/pushWechat")
-    public String pushWechat() throws WxErrorException {
-        WxMpTemplateMsgService templateMsgService = wxMpService.getTemplateMsgService();
-        WxMpTemplateMessage wxMpTemplateMessage = new WxMpTemplateMessage();
-        wxMpTemplateMessage.setTemplateId("RvvWaQI0at6oQ_X3U5x42-kXKdRXvu15x3QGeMWEzBo");
-        //微信公众号的对应openId
-        wxMpTemplateMessage.setToUser("");
-        WxMpTemplateMessage.MiniProgram miniProgram = new WxMpTemplateMessage.MiniProgram();
-        miniProgram.setAppid(wxMaService.getWxMaConfig().getAppid());
-        // miniProgram.setPagePath("pages/report/index");
-        miniProgram.setPagePath("pages/index");
-        wxMpTemplateMessage.setMiniProgram(miniProgram);
-        //防重入id。对于同一个openid + client_msg_id, 只发送一条消息,10分钟有效,超过10分钟不保证效果。若无防重入需求，可不填
-        //这里传报警记录的主键 确保一次报警只推送一次
-        wxMpTemplateMessage.setClientMsgId("11");
-        List<WxMpTemplateData> dataList = new ArrayList<>();
-        WxMpTemplateData wxMpTemplateData = new WxMpTemplateData();
-        wxMpTemplateData.setName("thing73");
-        wxMpTemplateData.setValue("SD400MP");
-        dataList.add(wxMpTemplateData);
-        WxMpTemplateData wxMpTemplateData2 = new WxMpTemplateData();
-        wxMpTemplateData2.setName("thing52");
-        wxMpTemplateData2.setValue("220kV I母压变");
-        dataList.add(wxMpTemplateData2);
-        WxMpTemplateData wxMpTemplateData3 = new WxMpTemplateData();
-        wxMpTemplateData3.setName("time3");
-        wxMpTemplateData3.setValue(DateUtil.now());
-        dataList.add(wxMpTemplateData3);
-        WxMpTemplateData wxMpTemplateData4 = new WxMpTemplateData();
-        wxMpTemplateData4.setName("thing46");
-        wxMpTemplateData4.setValue("数据中断");
-        dataList.add(wxMpTemplateData4);
-        WxMpTemplateData wxMpTemplateData5 = new WxMpTemplateData();
-        wxMpTemplateData5.setName("thing25");
-        wxMpTemplateData5.setValue("数据中断超过3小时，请检查！");
-        dataList.add(wxMpTemplateData5);
-        wxMpTemplateMessage.setData(dataList);
-        return templateMsgService.sendTemplateMsg(wxMpTemplateMessage);
+    private List<String> getOpenIdListByGroupId(Long groupId) {
+        List<String> openIdList = new ArrayList<>();
+        Set<Long> visitedIds = new HashSet<>();
+        collectOpenIds(groupId, openIdList, visitedIds);
+        return openIdList;
     }
-} 
+
+    /**
+     * 递归收集用户openId
+     *
+     * @param parentId 父节点ID
+     * @param openIdList openId列表
+     * @param visitedIds 已访问的ID集合（防止循环引用）
+     */
+    private void collectOpenIds(Long parentId, List<String> openIdList, Set<Long> visitedIds) {
+        if (parentId == null || visitedIds.contains(parentId)) {
+            return;
+        }
+        visitedIds.add(parentId);
+
+        // 获取该节点的所有子节点
+        List<UserGroup> children = userGroupService.getByParentUserId(parentId);
+
+        for (UserGroup child : children) {
+            Long userId = child.getUserId();
+            if (userId == null || visitedIds.contains(userId)) {
+                continue;
+            }
+
+            User user = userService.getById(userId);
+            if (user == null) {
+                continue;
+            }
+
+            // 如果是普通用户(type=1)，检查是否有mp_openid属性
+            if (UserType.USER.getValue() == user.getType()) {
+                UserProperties mpOpenIdProp = userPropertiesService.getByUserIdAndKey(userId, UserPropertiesConstant.KEY_MP_OPENID);
+                if (mpOpenIdProp != null && mpOpenIdProp.getValue() != null) {
+                    String openId = new String(mpOpenIdProp.getValue());
+                    if (StrUtil.isNotBlank(openId)) {
+                        openIdList.add(openId);
+                    }
+                }
+                visitedIds.add(userId);
+            } else if (UserType.USER_GROUP.getValue() == user.getType()) {
+                // 如果是用户组(type=2)，递归处理
+                collectOpenIds(userId, openIdList, visitedIds);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/global/ums/dto/AlarmPushDTO.java
@@ -0,0 +1,56 @@
+package com.global.ums.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * 告警推送请求DTO
+ * 用于微信公众号模板消息推送
+ */
+@Data
+public class AlarmPushDTO {
+
+    /**
+     * 用户组ID，将遍历该用户组下所有type=1且有mp_openid属性的用户进行推送
+     */
+    @NotNull(message = "userId不能为空")
+    private Long userId;
+
+    /**
+     * 防重入id，传报警记录的主键确保一次报警只推送一次
+     */
+    @NotBlank(message = "clientMsgId不能为空")
+    private String clientMsgId;
+
+    /**
+     * 所属系统 (thing73)
+     */
+    @NotBlank(message = "所属系统不能为空")
+    private String systemName;
+
+    /**
+     * 测点名称 (thing52)
+     */
+    @NotBlank(message = "测点名称不能为空")
+    private String pointName;
+
+    /**
+     * 告警时间 (time3)
+     * 如果不传，默认使用当前时间
+     */
+    private String alarmTime;
+
+    /**
+     * 告警类型 (thing46)
+     */
+    @NotBlank(message = "告警类型不能为空")
+    private String alarmType;
+
+    /**
+     * 异常原因 (thing25)
+     */
+    @NotBlank(message = "异常原因不能为空")
+    private String exceptionReason;
+}
